0e1595cbd0ae4ce64b36608cf2f9699e130c15ac
shoaibakhterqadri <github@4dmagic.pk>
2023-08-01 18:09:27 +0500

    one cart is completed

--- project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-recipes=[];
+recipes:Recipe[]=[
+  new Recipe("A Test Recipe","This is simply a test","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuUx1JDbk_xc6-4ipUf9liGj88hr9uitYLPQ&usqp=CAU")
+];
 }
