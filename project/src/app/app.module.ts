388c17cb36da9e81407abb0a0190ad6b4a02df12
shoaibakhterqadri <shoaibakhter181422@gmail.com>
2023-08-02 09:29:09 +0500

    Ingrediant Model is completed

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
@@ -20,6 +21,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     RecipeListComponent,
     RecipeItemComponent,
     ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
